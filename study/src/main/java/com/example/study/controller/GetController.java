bb2ac9dae9e15c94359e2b9aa835bdd50d711705
jaeho310 <chlwogh1993@gmail.com>
2020-11-04 16:21:54 +0900

    first commit

---
+++ study/src/main/java/com/example/study/controller/GetController.java
@@ -0,0 +1,32 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class GetController {
+
+    @RequestMapping(method = RequestMethod.GET, path ="/getMethod") // localhost:8080/api/getMethod
+    public String getRequest() {
+        return "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter")
+    public String getParameter(@RequestParam String id, @RequestParam String password) {
+
+        System.out.println("id : " +id);
+        System.out.println("password : " +password);
+
+        return id+password;
+    }
+
+    @GetMapping("/getMultiParameter")
+    public String getMultiParameter(SearchParam searchParam) {
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+        return "OK";
+    }
+}
