3dbea4ec50654ce14371e9fa07034ac57f31c26f
jaeho310 <chlwogh1993@gmail.com>
2020-11-15 13:23:58 +0900

    Add partnerRepositoryTest

---
+++ study/src/test/java/com/example/study/repository/PartnerRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.Partner;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartnerRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Test
+    public void create() {
+        String name = "Partner01";
+        String status = "REGISTERED";
+        String address = "서울시 강남구";
+        String callCenter = "070-1111-2222";
+        String partnerNumber = "070-1111-2222";
+        String businessNumber = "1234567890123";
+        String ceoName = "홍길동";
+        LocalDateTime registeredAt = LocalDateTime.now();
+        LocalDateTime createdAt = LocalDateTime.now();
+        String createdBy = "AdminServer";
+
+        Partner partner = new Partner();
+        partner.setName(name);
+        partner.setStatus(status);
+        partner.setAddress(address);
+    }
+
+    @Test
+    public void read() {
+
+    }
+}
