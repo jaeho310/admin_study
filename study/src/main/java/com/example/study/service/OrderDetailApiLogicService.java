bee813c34fccd53bd7c4fe2337f1f0aeba0d5438
jaeho310 <chlwogh1993@gmail.com>
2020-11-25 16:05:19 +0900

    Add OrderDetail controller, Service, Request, Response

---
+++ study/src/main/java/com/example/study/service/OrderDetailApiLogicService.java
@@ -0,0 +1,88 @@
+package com.example.study.service;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.OrderDetail;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.OrderDetailApiRequest;
+import com.example.study.model.network.response.OrderDetailApiResponse;
+import com.example.study.repository.ItemRepository;
+import com.example.study.repository.OrderDetailRepository;
+import com.example.study.repository.OrderGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderDetailApiLogicService implements CrudInterface<OrderDetailApiRequest, OrderDetailApiResponse> {
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    @Autowired
+    private OrderGroupRepository orderGroupRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Override
+    public Header<OrderDetailApiResponse> create(Header<OrderDetailApiRequest> request) {
+
+        OrderDetail orderDetail = OrderDetail.builder()
+                .id(request.getData().getId())
+                .status(request.getData().getStatus())
+                .arrivalDate(request.getData().getArrivalDate())
+                .quantity(request.getData().getQuantity())
+                .totalPrice(request.getData().getTotalPrice())
+                .orderGroup(orderGroupRepository.getOne(request.getData().getOrderGroupId()))
+                .item(itemRepository.getOne(request.getData().getItemId()))
+                .build();
+
+        OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
+        return response(newOrderDetail);
+    }
+
+    @Override
+    public Header<OrderDetailApiResponse> read(Long id) {
+        return orderDetailRepository.findById(id)
+                .map(orderDetail -> response(orderDetail))
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
+    }
+
+    @Override
+    public Header<OrderDetailApiResponse> update(Header<OrderDetailApiRequest> request) {
+        return orderDetailRepository.findById(request.getData().getId())
+                .map(orderDetail -> {
+                    orderDetail.setStatus(request.getData().getStatus())
+                            .setArrivalDate(request.getData().getArrivalDate())
+                            .setQuantity(request.getData().getQuantity())
+                            .setTotalPrice(request.getData().getTotalPrice());
+                    return orderDetail;
+                })
+                .map(orderDetail -> orderDetailRepository.save(orderDetail))
+                .map(this::response)
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return orderDetailRepository.findById(id)
+                .map(orderDetail -> {
+                    orderDetailRepository.delete(orderDetail);
+                    return Header.OK();
+                })
+                .orElseGet(()->Header.ERROR("데이터 없음"));
+    }
+
+    private Header<OrderDetailApiResponse> response(OrderDetail orderDetail) {
+        OrderDetailApiResponse body = OrderDetailApiResponse.builder()
+                .id(orderDetail.getId())
+                .status(orderDetail.getStatus())
+                .arrivalDate(orderDetail.getArrivalDate())
+                .quantity(orderDetail.getQuantity())
+                .totalPrice(orderDetail.getTotalPrice())
+                .orderGroupId(orderDetail.getOrderGroup().getId())
+                .itemId(orderDetail.getItem().getId())
+                .build();
+        return Header.OK(body);
+
+    }
+}
